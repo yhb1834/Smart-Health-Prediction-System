755ee0237a140042d3f57eed4ba17e1e14e4325d
sohyunbae1231 <sht06025@gmail.com>
2021-05-20 01:42:35 +0900

    [webpage]-[doctor_patient]-[templates]-[doctor_patient]의 main.html과 http://127.0.0.1:8000/main/을 연동시킴

--- webpage/doctor_patient/settings.py
+++ webpage/doctor_patient/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'ad',
+    #'doctor',
+    #'patient',
+    'doctor_patient',
 ]
 
 MIDDLEWARE = [
--- webpage/doctor_patient/urls.py
+++ webpage/doctor_patient/urls.py
@@ -14,21 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'), # 통합 메인 페이지
+    path('main/', views.main),
 
     # admin urls
-    path('admin/main', views.admin_main, name='admin_main'), # admin 메인 페이지
-    path('admin/signup', views.admin_signup, name='admin_signup'), # admin 회원가입
-    path('admin/login', views.admin_login, name='admin_login'), # admin 로그인
-    path('admin/doctor-certify', views.admin_doctor_certify, name='admin_doctor_certify'), # admin 의사 인증
-    path('admin/disease', views.admin_disease, name='admin_disease'), # admin 질병 DB 관리
-    path('admin/feedback', views.admin_feedback, name='admin_feedback'), # admin feedback
-    path('admin/doctor-profile', views.admin_doctor_profile, name='admin_doctor_profile'), # admin 의사 프로필 DB 관리
-    path('admin/patient-profile', views.admin_patient_profile, name='admin_patient_profile'), # admin 환자 프로필 DB 관리
+    #path('admin/main', views.admin_main, name='admin_main'), # admin 메인 페이지
+    #path('admin/signup', views.admin_signup, name='admin_signup'), # admin 회원가입
+    #path('admin/login', views.admin_login, name='admin_login'), # admin 로그인
+    #path('admin/doctor-certify', views.admin_doctor_certify, name='admin_doctor_certify'), # admin 의사 인증
+    #path('admin/disease', views.admin_disease, name='admin_disease'), # admin 질병 DB 관리
+    #path('admin/feedback', views.admin_feedback, name='admin_feedback'), # admin feedback
+    #path('admin/doctor-profile', views.admin_doctor_profile, name='admin_doctor_profile'), # admin 의사 프로필 DB 관리
+    #path('admin/patient-profile', views.admin_patient_profile, name='admin_patient_profile'), # admin 환자 프로필 DB 관리
 
     # doctor urls
 
