e1bab29520ec49907b612bb885433c03d9571ccb
sohyunbae1231 <sht06025@gmail.com>
2021-05-22 23:00:24 +0900

    static 위치 새로 추가 & urls, views, settings, forms 수정 & load static 완료

--- webpage/doctor_patient/urls.py
+++ webpage/doctor_patient/urls.py
@@ -18,28 +18,26 @@ from django.urls import path, include
 from . import views, patient_views
 
 urlpatterns = [
+    #admin urls
     path('admin/', admin.site.urls),
-    path('ad/main/', views.ad_main),
-    path('ad/login/', views.ad_login),
-    path('ad/signup/', views.ad_signup),
+    path('ad/main/', views.ad_main), # admin 메인 페이지
+    path('ad/login/', views.ad_login), # admin 로그인
+    path('ad/signup/', views.ad_signup), # admin 회원가입
+    #path('adm/doctor-certify', views.ad_doctor_certify), # admin 의사 인증
+    #path('ad/disease', views.admin_disease), # admin 질병 DB 관리
+    #path('ad/feedback', views.admin_feedback), # admin feedback
+    #path('ad/doctor-profile', views.admin_doctor_profile), # admin 의사 프로필 DB 관리
+    #path('ad/patient-profile', views.admin_patient_profile), # admin 환자 프로필 DB 관리
 
+    #doctor urls
     path('doctor/main/', views.doctor_main),
     path('doctor/login/', views.doctor_login),
     path('doctor/signup/', views.doctor_signup),
 
+    #patient urls
     path('patient/main', patient_views.pa_main),
     path('patient/login/', patient_views.pa_login),
     path('patient/signup/', patient_views.pa_signup),
-
-    # admin urls
-    #path('admin/main', views.admin_main, name='admin_main'), # admin 메인 페이지
-    #path('admin/signup', views.admin_signup, name='admin_signup'), # admin 회원가입
-    #path('admin/login', views.admin_login, name='admin_login'), # admin 로그인
-    #path('admin/doctor-certify', views.admin_doctor_certify, name='admin_doctor_certify'), # admin 의사 인증
-    #path('admin/disease', views.admin_disease, name='admin_disease'), # admin 질병 DB 관리
-    #path('admin/feedback', views.admin_feedback, name='admin_feedback'), # admin feedback
-    #path('admin/doctor-profile', views.admin_doctor_profile, name='admin_doctor_profile'), # admin 의사 프로필 DB 관리
-    #path('admin/patient-profile', views.admin_patient_profile, name='admin_patient_profile'), # admin 환자 프로필 DB 관리
 
     # doctor urls
 
