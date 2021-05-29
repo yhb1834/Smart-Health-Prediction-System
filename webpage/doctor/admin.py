fcfed5b56dbe36351bf137a741b3196e92a20988
JJPPH <61501348+JJPPH@users.noreply.github.com>
2021-05-29 22:52:45 +0900

    Add files via upload

--- webpage/doctor/admin.py
+++ webpage/doctor/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Doctor_user
+from .models import Doctor_user,Patient_list
 
 
 #장고 admin 페이지에서 그룹 관리할 때 쓰는 거임 
-@admin.register(Doctor_user)
+
 class Doctor_user_admin(admin.ModelAdmin):
     list_display = (
         'username',
@@ -11,3 +11,13 @@ class Doctor_user_admin(admin.ModelAdmin):
         'registered_date',
         'email'
     )
+    
+#@admin.register(Patient_list)
+class Patient_list_Admin(admin.ModelAdmin):
+    list_display = ('doctor_name', 
+                    'patient_name', 
+                    'symptom', 
+                    'date')
+    
+admin.site.register(Doctor_user, Doctor_user_admin)
+admin.site.register(Patient_list, Patient_list_Admin)
