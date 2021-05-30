9652ebefbd173382c8c264ce5b37ba6fab305952
sohyunbae1231 <sht06025@gmail.com>
2021-05-31 00:04:19 +0900

    동준이 대신 forms models, views 수정

--- webpage/doctor_patient/views.py
+++ webpage/doctor_patient/views.py
@@ -2,10 +2,7 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from doctor_patient.forms import AdUserForm, AdLoginForm, DoctorUserForm, QuestionForm # 여기 부분에 forms.py에 넣어져 있는 것들 꼭 추가!!!
 from django.utils import timezone
-
-import sys,os
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from doctor.models import Feedback
+from .models import User
 
 # Create your views here.
 
@@ -36,8 +33,14 @@ def ad_signup(request):
        if request.method == "POST":
            form = AdUserForm(request.POST)
            if form.is_valid():
-               #form.setAdmin() is_admin 값 어떻게 바꾸냐???? 일단 이건 안되는데
                form.save()
+               '''
+               어떻게 하면 값을 ture 할 수있는지 연습 인데 안되네 ㅠㅠㅠㅠ
+               user_id = request.session.get('username')
+               userobj = User.objects.get(username=user_id)
+               if not userobj.is_admin:
+                   userobj.is_admin = False 
+                '''
                username = form.cleaned_data.get('username')
                raw_password = form.cleaned_data.get('password1')
                user = authenticate(username=username, password=raw_password)
@@ -53,25 +56,16 @@ def ad_doctor_certify(request):
 
 # admin feedback 리스트 페이지
 def ad_feedback(request):
-    feedback_list = Feedback.objects.all()
-    return render(request, 'ad/feedback.html',{"feedback_list":feedback_list})
+    return render(request, 'ad/feedback.html')
 
 # admin feedback 쓰기 페이지
 def ad_feedback_write(request):
-
     if request.method == "POST":
         form = QuestionForm(request.POST)
         if form.is_valid():
-            feedback = Feedback()
-            feedback.username = "admin"
-            feedback.content = form.cleaned_data['content']
-            feedback.title = form.cleaned_data['title']
-            feedback.position = "admin"
+            feedback = form.save(commit=False)
+            feedback.create_date = timezone.now()
             feedback.save()
-            
-            #feedback = form.save(commit=False)
-            #feedback.create_date = timezone.now()
-            #feedback.save()
             return redirect('../feedback')
     else:
         form = QuestionForm()
@@ -79,4 +73,37 @@ def ad_feedback_write(request):
     return render(request, 'ad/feedbackwrite.html', context)
 
 
+# 임시로 doctor_관련 view들 추가하여 작업중 (이상진)
+# doctor subgroup의 임의 수정가능.
+def doctor_main(request):
+    return render(request, 'doctor/main.html')
 
+def doctor_login(request):
+    return render(request, 'doctor/login.html')
+
+def doctor_signup(request):
+    #계정 생성
+       if request.method == "POST":
+           form = DoctorUserForm(request.POST)
+           if form.is_valid():
+               form.save()
+               username = form.cleaned_data.get('username')
+               raw_password = form.cleaned_data.get('password1')
+               user = authenticate(username=username, password=raw_password)
+               login(request, user)
+               return redirect('index')
+       else:
+           form = DoctorUserForm()
+       return render(request, 'doctor/signup.html', {'form': form})
+
+def doctor_feedback(request):
+    return render(request, 'doctor/feedback.html')
+
+def doctor_patient_list(request):
+    return render(request, 'doctor/patient-list.html')
+
+def doctor_prescription(request):
+    return render(request, 'doctor/prescription.html')
+
+def doctor_reservation(request):
+    return render(request, 'doctor/reservation.html')
